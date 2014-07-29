a5e49c6c7ce3e362b75c0221155ce393f6d32bda
Jakub Lewandowski <jakub.lewandowski@gmail.com>
2014-07-29 20:00:41 +0100

    node now stores only next and num_pages

--- src/mm/phys_allocator.c
+++ src/mm/phys_allocator.c
@@ -25,29 +25,33 @@
 
 struct node
 {
-    void *begin;
-    void *end;
     struct node *next;
+    size_t num_pages;
 };
+
+
+static inline const void *node_end(const struct node *node)
+{
+    return (const void *)node + node->num_pages * PAGE_SIZE;
+}
+
+
+static inline void try_merge_with_next(struct node *node)
+{
+    if (node->next && node_end(node) == node->next)
+    {
+        node->num_pages += node->next->num_pages;
+        node->next = node->next->next;
+    }
+}
 
 
 static struct node *first_node;
 
 
-static void try_merge_with_next(struct node *node)
-{
-    if (node->end == node->next)
-    {
-        struct node *next_node = node->next;
-        node->next = next_node->next;
-        node->end = next_node->end;
-    }
-}
-
-
 static inline void insert_node(struct node *node)
 {
-    if (first_node == NULL || node->begin < (void *)first_node)
+    if (first_node == NULL || node < first_node)
     {
         node->next = first_node;
         try_merge_with_next(node);
@@ -58,16 +62,15 @@ static inline void insert_node(struct node *node)
     struct node *current = first_node;
     while (current->next != NULL)
     {
-        struct node *next_node = current->next;
-        if (node->begin < next_node->begin)
+        if (node < current->next)
         {
             break;
         }
-        current = next_node;
+        current = current->next;
     }
 
     node->next = current->next;
-    current->next = (struct node *)node->begin;
+    current->next = node;
     try_merge_with_next(node);
     try_merge_with_next(current);
 }
@@ -78,7 +81,8 @@ void print_phys_mem(void)
     const struct node *node = first_node;
     while (node)
     {
-        printf("Node [0x%lx, 0x%lx, 0x%lx)\n", (uintptr_t)node->begin, (uintptr_t)node->end, (uintptr_t)node->next);
+        printf("Node [0x%lx, 0x%lx), 0x%lx\n", (uintptr_t)node,
+               (uintptr_t)(node_end(node)), (uintptr_t)node->next);
         node = node->next;
     }
 }
@@ -99,8 +103,7 @@ void init_phys_allocator(const struct address_range *regions, int num_regions)
             continue;
         }
         struct node *node = (struct node *)mm_va(beg);
-        node->begin = (void *)mm_va(beg);
-        node->end = (void *)mm_va(end);
+        node->num_pages = (end - beg) / PAGE_SIZE;
         node->next = NULL;
         insert_node(node);
     }
@@ -113,18 +116,17 @@ void *alloc_pages(size_t num_pages)
     if (first_node)
     {
         res = first_node;
-        if (first_node->end - first_node->begin == PAGE_SIZE)
+        if (first_node->num_pages == 1)
         {
             first_node = first_node->next;
         }
         else
         {
-            void *new_begin = first_node->begin + PAGE_SIZE;
-            void *new_end = first_node->end;
+            void *new_begin = (void *)first_node + PAGE_SIZE;
+            size_t new_num_pages = first_node->num_pages - 1;
             struct node *new_next = first_node->next;
             first_node = (struct node *)new_begin;
-            first_node->begin = new_begin;
-            first_node->end = new_end;
+            first_node->num_pages = new_num_pages;
             first_node->next = new_next;
         }
     }
@@ -135,8 +137,7 @@ void *alloc_pages(size_t num_pages)
 void free_pages(void *virt_address, size_t num_pages)
 {
     struct node *new_node = (struct node *)virt_address;
-    new_node->begin = virt_address;
-    new_node->end = virt_address + PAGE_SIZE;
+    new_node->num_pages = num_pages;
     new_node->next = NULL;
     insert_node(new_node);
 }
